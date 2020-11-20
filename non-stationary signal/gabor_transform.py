2da46eb7a2a390fe3d5df44537226ed949097e85
waskitopranowo <57626823+waskitopranowo@users.noreply.github.com>
2020-11-20 08:52:00 +0700

    Update gabor_transform.py

--- non-stationary signal/gabor_transform.py
+++ non-stationary signal/gabor_transform.py
@@ -9,13 +9,16 @@ time = data[np.arange(0, data.shape[0], dt), 0]
 sig = data[np.arange(0, data.shape[0], dt), 1]
 
 windowms = 101  # gabor transform window in samples
+overlap = 0.75 # window overlapping (in fraction)
 
 window = int(np.round(windowms/dt))
 zpad = int(np.floor(window/2))  # number of zero padding
 sigzpad = np.hstack((np.zeros(zpad), sig, np.zeros(zpad)))
 
+noverlap = overlap*window
+nshift = int(1 - noverlap)
 stft = []
-for i in np.arange(0, len(sig), 1):
+for i in np.arange(0, len(sig), nshift):
     movwind = np.zeros(len(sigzpad))
     movwind[i:i + window] = windows.gaussian(window, window/6)
     sigwin = np.multiply(sigzpad, movwind)
